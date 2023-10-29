e389f2aae0a902ac8a657d2bb50eb469a3045b8a
Matt Lavis <matthew.lavis@gmail.com>
2023-10-29 14:25:12 +0000

    Linting

--- classes/functions.py
+++ classes/functions.py
@@ -2,15 +2,19 @@ def parse_date(d):
     d2 = d[6:8] + "/" + d[4:6] + "/" + d[0:4]
     return (d2)
 
+
 def get_nodes(path):
     pass
 
+
 def xml_to_xlsx_filename(filename):
+    # filename = filename
     parts = filename.split("T")
     part0 = parts[0]
     parts = part0.split("-")
     part1 = parts[1]
-    excel_filename = "CDS updates " + part1[0:4] + "-" + part1[4:6] + "-" + part1[6:] + ".xlsx"
+    excel_filename = "CDS updates " + \
+        part1[0:4] + "-" + part1[4:6] + "-" + part1[6:] + ".xlsx"
     return (excel_filename)
 
 
@@ -84,6 +88,7 @@ def get_measurement_unit(s):
         # return "1000 kg" # 1005900020
     else:
         return s
+
 
 def get_qualifier(s):
     sQualDesc = ""
